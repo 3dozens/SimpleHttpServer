d9797541e8a7cf1380badd951d6a1e3d283bab2d
kento_hayasaka <thunders.1028@gmail.com>
2017-03-05 22:24:58 +0900

    Add socket communication

--- src/http/Main.java
+++ src/http/Main.java
@@ -1,8 +1,21 @@
 package http;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 
-	public static void main(String args[]) {
+	public static void main(String args[]) throws Exception {
 		System.out.println("start >>");
+		
+		try (
+				ServerSocket server = new ServerSocket(8080);
+				Socket socket = server.accept(); // このメソッドはブロッキング処理なので、接続があるまでプログラムはこの場所で停止する。
+				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+				) {
+			in.lines().forEach(System.out::println);
+		}
 	}
 }
